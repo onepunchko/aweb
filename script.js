7516c9b5fc25002cb07f9480db654f89b6893f9d
onepunchko <ylily7771@gmail.com>
2024-10-07 13:30:28 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+document.getElementById('menu-button').addEventListener('click', function() {
+    const sidebar = document.getElementById('sidebar');
+    
+	// 切换侧边栏显示状态
+	if (sidebar.style.left === '0px') {
+        sidebar.style.left = '-250px'; // 隐藏侧边栏
+	} else {
+        sidebar.style.left = '0'; // 显示侧边栏
+	}
+});
+
+// 使用 Fetch API 获取 README.md 内容并插入到页面中。
+fetch('./readme.md')
+.then(response => response.text())
+.then(data => {
+	document.getElementById('readme-content').textContent = data;
+
+	const searchResults = document.getElementById('search-results');
+	const lines = data.split('\n');
+
+	lines.forEach((line, index) => {		
+	    if(line.trim()) {	
+	        const li = document.createElement('li');		
+	        li.textContent = line.trim();		
+	        li.addEventListener('click', () => {			
+	            window.scrollTo(0, index * lineHeight); // 根据行高滚动到相应位置			
+            });		
+            searchResults.appendChild(li);	
+	    }	
+	});
+})
+.catch(error => console.error(error));
